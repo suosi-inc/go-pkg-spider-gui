0bf7e68b6aa5b0aa0eaa0549d28300041018ec8a
smallmenu <smallmenu@gmail.com>
2023-01-12 15:54:10 +0800

    * init

--- lazarus/unit1.pas
+++ lazarus/unit1.pas
@@ -5,14 +5,49 @@ unit Unit1;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, ComCtrls;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, ComCtrls,
+  StdCtrls, Menus, Buttons;
 
 type
 
   { TFormMain }
 
   TFormMain = class(TForm)
+    BtnRequest: TButton;
+    Button1: TButton;
+    CheckBox1: TCheckBox;
+    CheckBox2: TCheckBox;
+    CheckBox3: TCheckBox;
+    CheckBox4: TCheckBox;
+    Edit1: TEdit;
+    Edit2: TEdit;
+    Edit3: TEdit;
+    EditRequestUa: TEdit;
+    EditRequestUrl: TEdit;
+    GroupBoxRequest: TGroupBox;
     ImageListToolBar: TImageList;
+    Label1: TLabel;
+    Label3: TLabel;
+    Label4: TLabel;
+    LabelRequestUa: TLabel;
+    Label2: TLabel;
+    LabelRequestUrl: TLabel;
+    Memo1: TMemo;
+    MemoRequest: TMemo;
+    MemoDebug: TMemo;
+    MenuDebugCopy: TMenuItem;
+    MenuDebugClear: TMenuItem;
+    PageControl: TPageControl;
+    PanelRequestView: TPanel;
+    PanelRequestController: TPanel;
+    PanelDebug: TPanel;
+    PopupMenuDebug: TPopupMenu;
+    SplitterRequest: TSplitter;
+    SplitterDebug: TSplitter;
+    TabSheetContent: TTabSheet;
+    TabSheetLink: TTabSheet;
+    TabSheetDomain: TTabSheet;
+    TabSheetRequest: TTabSheet;
     ToolBar: TToolBar;
     ToolBtnSplit1: TToolButton;
     ToolBtnRequest: TToolButton;
@@ -26,6 +61,12 @@ type
     ToolBtnSplit5: TToolButton;
     ToolBtnTool: TToolButton;
     ToolBtnSplit6: TToolButton;
+    procedure GroupBox1Click(Sender: TObject);
+    procedure Label2Click(Sender: TObject);
+    procedure MenuDebugClearClick(Sender: TObject);
+    procedure MenuDebugCopyClick(Sender: TObject);
+    procedure MenuToolLanguageClick(Sender: TObject);
+    procedure ToolBtnDebugClick(Sender: TObject);
   private
 
   public
@@ -38,6 +79,49 @@ var
 implementation
 
 {$R *.lfm}
+
+{ TFormMain }
+
+procedure TFormMain.MenuToolLanguageClick(Sender: TObject);
+begin
+
+end;
+
+procedure TFormMain.ToolBtnDebugClick(Sender: TObject);
+begin
+  if ToolBtnDebug.Down then
+     begin
+        SplitterDebug.Visible:=True;
+        PanelDebug.Visible:=True;
+        PageControl.AnchorSideBottom.Control:=SplitterDebug
+     end
+     else
+     begin
+        SplitterDebug.Visible:=False;
+        PanelDebug.Visible:=False;
+        PageControl.AnchorSideBottom.Control:=FormMain;
+     end;
+end;
+
+procedure TFormMain.MenuDebugCopyClick(Sender: TObject);
+begin
+  MemoDebug.CopyToClipboard;
+end;
+
+procedure TFormMain.MenuDebugClearClick(Sender: TObject);
+begin
+  MemoDebug.Text:=''
+end;
+
+procedure TFormMain.GroupBox1Click(Sender: TObject);
+begin
+
+end;
+
+procedure TFormMain.Label2Click(Sender: TObject);
+begin
+
+end;
 
 end.
 
